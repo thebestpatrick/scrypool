0fc35acae5bee088d72010900b60bde1861de92b
Patrick Ingham <emmanuel.goldstien@gmail.com>
2014-07-26 00:26:13 -0600

    bringing the rest of the program under revision control

--- futzing.py
+++ futzing.py
@@ -2,6 +2,9 @@
 
 from charactercreator import yaml_create_character
 import yaml
-a = yaml_create_character("human", "barbarian", "none")
-f = open("charactersheets/blah.yml", 'w')
-f.write(a)
+
+#a = yaml.load(open("classes/barbarian.yml"))
+#b = yaml.load(open("races/human.yml"))
+#speclist = a["level 1"]["special"] + b["race specials"]
+#print(parse_specials(speclist))
+print(yaml.dump(yaml_create_character("human", "barbarian", "null"), default_flow_style=False))
