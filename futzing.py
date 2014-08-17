519440de0d9ceceb25f9db6af95ac89262e1b14e
Patrick Ingham <emmanuel.goldstien@gmail.com>
2014-08-17 14:06:15 -0600

    added elf race file

--- futzing.py
+++ futzing.py
@@ -5,5 +5,5 @@ import yaml
 #a = yaml.load(open("classes/barbarian.yml"))
 #b = yaml.load(open("races/human.yml"))
 
-print(yaml.dump(yaml_create_character("half-orc", "barbarian", ["mobility", ]), default_flow_style=False))
+print(yaml.dump(yaml_create_character("elf", "barbarian", ["mobility", ]), default_flow_style=False))
 
