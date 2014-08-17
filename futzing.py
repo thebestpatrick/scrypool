19a2577f6b4186915e55ba2243190cc7364e55dc
Patrick Ingham <emmanuel.goldstien@gmail.com>
2014-08-17 14:19:45 -0600

    added dwarf race file

--- futzing.py
+++ futzing.py
@@ -5,5 +5,5 @@ import yaml
 #a = yaml.load(open("classes/barbarian.yml"))
 #b = yaml.load(open("races/human.yml"))
 
-print(yaml.dump(yaml_create_character("elf", "barbarian", ["mobility", ]), default_flow_style=False))
+print(yaml.dump(yaml_create_character("dwarf", "barbarian", ["mobility", ]), default_flow_style=False))
 
