f900ee0ecda8b2ab03f47a0178e09d884f0802a5
Patrick Ingham <emmanuel.goldstien@gmail.com>
2014-12-05 14:34:12 -0700

    Added rudimentary wizard support

--- futzing.py
+++ futzing.py
@@ -6,4 +6,4 @@ import random
 #a = yaml.load(open("classes/barbarian.yml"))
 #b = yaml.load(open("races/human.yml"))
 # For testing everything
-print(yaml.dump(yaml_create_character("halfling", "monk"), default_flow_style=False))
+print(yaml.dump(yaml_create_character("halfling", "wizard"), default_flow_style=False))
